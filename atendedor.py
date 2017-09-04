99299b5997c9bba11c1b8e08e9d5159742f00ed6
Facundo <facundogcuba@gmail.com>
2017-09-04 18:40:18 -0300

    Se mejoro la salida de atendedor y se agrego la clase Visor, que muestra la lista ordenada de todo los que se deben atender y busca a Persona por nombre o dni.

--- atendedor.py
+++ atendedor.py
@@ -1,12 +1,11 @@
 class Persona():
     def __init__(self, nom, doc):
-        self.nompre = nom
+        self.nombre = nom
         self.dni = doc
         self.numeroAsignado = 0
 
     def darNum(self):
         return self.numeroAsignado
-
 
 class ColaEspera():
     def __init__(self, n):
@@ -25,9 +24,53 @@ class ColaEspera():
         p = self.pers.pop(0)
         return p.numAsignado
 
+class Visor():
+    def __init__(self):
+        pass
+
+    def mostrarCola(self):
+        print("Nombre", end=" ")
+        print("DNI", end=" ")
+        print("Turno")
+        for p in cola.pers:
+            print(p.nombre, end=" ")
+            print(p.dni, end=" ")
+            print(p.numAsignado)
+
+    def buscarPersona(self, p):
+        for i in cola.pers:
+            if p in i.nombre:
+                print(i.nombre, end=" ")
+                print(i.dni, end=" ")
+                print(i.numAsignado)
+            elif p in i.dni:
+                print(i.nombre, end=" ")
+                print(i.dni, end=" ")
+                print(i.numAsignado)
+
 cola = ColaEspera(3)
-n = input("Ingrese su nombre: ")
-d = input("Ingrese su numero de DNI: ")
-print(cola.agregar(Persona(n, d)))
+for i in range(2):
+    n = input("Ingrese su nombre: ")
+    d = input("Ingrese su numero de DNI: ")
+    turno = cola.agregar(Persona(n, d))
+    print("Bienvenido", n, "su turno es:", turno)
 
+ver = Visor()
+print("")
+ver.mostrarCola()
+print("")
+ver.buscarPersona(str(111))
 
+"""""
+for i in cola.pers:
+    print(i.nombre, end=" ")
+    print(i.dni, end=" ")
+    print(i.numAsignado)
+
+print("El siguiente es:", cola.atenderProx())
+
+for i in cola.pers:
+    print(i.nombre, end=" ")
+    print(i.dni, end=" ")
+    print(i.numAsignado)
+"""
