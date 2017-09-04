dca0d3ee7ea795de562510a64a56b0006d9a1226
Facundo <facundogcuba@gmail.com>
2017-09-04 20:12:06 -0300

    Se agrego el ejercicio del anotador de notas. Quedan implementar funciones.

---
+++ anotador.py
@@ -0,0 +1,91 @@
+import datetime
+
+class Nota:
+    def __init__(self):
+        self.id = 0
+        self.titulo = ""
+        self.texto = ""
+        self.fecha = ""
+
+class Block:
+    def __init__(self):
+        self.notas = []
+        self.prox_id = 0
+
+    def agregar(self, nota):
+        self.notas.append(nota)
+        self.prox_id = self.prox_id + 1
+
+    def modificar(self, id, nota):
+        pass
+
+    def buscar(self, id):
+        pass
+
+    def borrar(self, id):
+        pass
+
+class Menu:
+    def __init__(self):
+        self.miBlock = Block()
+        self.funciones = {
+            "1": self.listarNotas,
+            "2": self.agregarNota,
+            "3": self.modificarNota,
+            "4": self.borrarNota,
+            "5": self.buscarNota
+        }
+
+    def listarNotas(self):
+        print("Listado de notas del block")
+        for n in self.miBlock.notas:
+            print("=" * 40)
+            print(n.id, end=' - ')
+            print(n.titulo, end=" - ")
+            print(n.fecha)
+            print(n.texto)
+            print()
+
+    def mostrarMenu(self):
+        while True:
+            print("Ingrese opción:")
+            print("""
+                1. Listar notas
+                2. Agregar Nota
+                3. Modificar Nota
+                4. Borrar Nota
+                5. Buscar Nota
+                6. Salir
+            """)
+            opc = self.ingresarOpcion()
+            if opc == 6:
+                break
+            else:
+                funcion = self.funciones[opc]
+
+                if funcion:
+                    funcion()
+
+    def ingresarOpcion(self):
+        return input("ingrese opción >")
+
+    def agregarNota(self):
+        n = Nota()
+        n.titulo = input("Ingrese título: ")
+        n.texto = input("Ingrese texto")
+        n.id = self.miBlock.prox_id
+        n.fecha = datetime.datetime.today()
+        self.miBlock.agregar(n)
+
+    def borrarNota(self):
+        pass
+
+    def modificarNota(self):
+        pass
+
+    def buscarNota(self):
+        pass
+
+if __name__ == '__main__':
+    mnu = Menu()
+    mnu.mostrarMenu()
